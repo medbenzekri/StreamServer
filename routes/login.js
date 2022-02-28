6b7df7676c36ecb738b82f9ca3c06b143981488b
medbenzekri <medbenzekri219@gmail.com>
2022-02-28 19:04:20 +0100

    init commit

---
+++ routes/login.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* auth user  */
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+  res.send('hello');
+});
+
+module.exports = router;
