10c0cbba44a0b1b9f4964f2c056b041476d36c13
medbenzekri <medbenzekri219@gmail.com>
2022-03-03 08:56:06 +0100

    get duration produce an error

--- routes/handlers/video_handler.js
+++ routes/handlers/video_handler.js
@@ -1,10 +1,39 @@
-var Minio = require('minio')
-
- function getVideo(video){
+var {spawn}= require('child_process');
+const byteSize = require('byte-size');
+const stream = require('stream');
+const Client= require('../../minio_client');
+var bucket= require('config').get("Bucket");
+ function Videoinfo(video){
    
-   console.log(video)
-  
+   
+  return {
+    title:video.name ,
+    size:`${byteSize(video.size)}`,
+    duration:getduration(video.name)
+
+  }
 
 }
 
-module.exports = { getVideo };
+async function getduration(name){
+  
+  var offset=0
+    var ffmpeg= spawn('ffmpeg', [
+      '-i', '-',
+      '-'
+    ]);
+    await (await Client.getPartialObject(bucket,name,offset)).pipe(ffmpeg.stdin,{end:true})
+    .on("close",()=>{
+      console.log("stream is closed")
+    })
+    ffmpeg.stdout.on("data",(output)=>{
+
+      return console.log(output);
+    })
+    .on("end",()=>ffmpeg.kill()).on("error",()=>console.log("error\n"))
+  
+      
+    
+  }
+
+module.exports = { Videoinfo };
