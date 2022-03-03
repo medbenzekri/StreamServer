fb7afdbf4eeb559a9f77709ba7433ba3187d2962
medbenzekri <medbenzekri219@gmail.com>
2022-03-03 08:56:37 +0100

    acess to video router fixed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-var videoRouter = require('./routes/video');
+var videoRouter = require('./routes/video').router;
 var app = express();
 
 app.use(logger('dev'));
