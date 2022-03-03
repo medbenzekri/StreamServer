69267383970b77cedea3915afe91f1223ad3a7c2
Bentaib Mohammed Yazid <78019553+mohammedyazid@users.noreply.github.com>
2022-03-03 17:47:08 +0100

    Get Video Duration Added

--- routes/handlers/video_handler.js
+++ routes/handlers/video_handler.js
@@ -3,37 +3,27 @@ const byteSize = require('byte-size');
 const stream = require('stream');
 const Client= require('../../minio_client');
 var bucket= require('config').get("Bucket");
- function Videoinfo(video){
-   
-   
+const { getVideoDurationInSeconds } = require('get-video-duration')
+ 
+function Videoinfo(video){   
   return {
     title:video.name ,
     size:`${byteSize(video.size)}`,
     duration:getduration(video.name)
-
   }
-
 }
 
 async function getduration(name){
   
-  var offset=0
-    var ffmpeg= spawn('ffmpeg', [
-      '-i', '-',
-      '-'
-    ]);
-    await (await Client.getPartialObject(bucket,name,offset)).pipe(ffmpeg.stdin,{end:true})
-    .on("close",()=>{
-      console.log("stream is closed")
-    })
-    ffmpeg.stdout.on("data",(output)=>{
-
-      return console.log(output);
-    })
-    .on("end",()=>ffmpeg.kill()).on("error",()=>console.log("error\n"))
-  
-      
-    
+var offset = 0;
+var minutes,seconds;
+getVideoDurationInSeconds(await Client.getPartialObject(bucket,name,offset)).then((duration) => {
+  duration = (duration/60);
+  minutes = duration - (duration % 1);
+  seconds = (duration % 1)*60;
+  duration = minutes +":"+seconds.toFixed(0);
+  return console.log(duration);
+})
   }
 
 module.exports = { Videoinfo };
