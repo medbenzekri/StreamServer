9423a9730b0432d130614bb562df28ae3602f8d3
Bentaib Mohammed Yazid <78019553+mohammedyazid@users.noreply.github.com>
2022-03-06 10:02:09 +0100

    Update

--- routes/handlers/video_handler.js
+++ routes/handlers/video_handler.js
@@ -3,14 +3,17 @@ const byteSize = require('byte-size');
 const stream = require('stream');
 const Client= require('../../minio_client');
 var bucket= require('config').get("Bucket");
-const { getVideoDurationInSeconds } = require('get-video-duration')
+const genThumbnail = require('simple-thumbnail');
+const { getVideoDurationInSeconds } = require('get-video-duration');
+const pathToFfmpeg = require('ffmpeg-static');
 
 async function Videoinfo(video){   
   
   return {
     title:video.name ,
     size:`${byteSize(video.size)}`,
-    duration:await getduration(video.name)
+    duration:await getduration(video.name),
+    thumbnail:await download(video.name)
   }
 }
 
@@ -25,4 +28,17 @@ return `${String(~~(duration/60)).padStart(2,"0")}:${String(duration%60).padStar
 
 
 }
+
+async function download (name) {
+  const ffmpeg = require('ffmpeg-static')
+  var offset = 0;
+  var output = new stream.Writable();
+  output = await genThumbnail(await Client.getPartialObject(bucket,name,offset),'150x',{
+    path:ffmpeg.path,
+  })
+  console.log('Done')
+  return output
+  
+}
+
 module.exports = { Videoinfo };
