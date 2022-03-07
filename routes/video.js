6f0e851515568cad6de6215bcca46f179adb76d3
medbenzekri <medbenzekri219@gmail.com>
2022-03-07 23:10:38 +0100

    thumbnail route has been added

--- routes/video.js
+++ routes/video.js
@@ -4,6 +4,7 @@ var {Videoinfo}=require('./handlers/video_handler');
 var Client=require("../minio_client");
 var bucket= require('config').get("Bucket");
 var async = require("async");
+const genThumbnail = require('simple-thumbnail')  
 
 
 router.get('/',async function(req, res, next) {
@@ -15,6 +16,14 @@ router.get('/',async function(req, res, next) {
       on("data",streamQueue.push);
       streamQueue.drain(()=>res.json(videos))
   });
+
+router.get('/thumbnail/:id',async(req,res,next)=>{
+  var id=decodeURIComponent(req.params.id)
+  await genThumbnail(await Client.getPartialObject(bucket,id,0),res,"150x100")
+  res.on("finish",res.end)
+  
+})
+
 
   exports.router=router;
   
